56b748f5142c68ddbff9b8b540804aef065abeb0
Alex Averbuch <alex.averbuch@gmail.com>
2010-05-19 18:14:49 +0200

    + added GISGenerateOperations & GISLoadOperations. just provide main() for running from console
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

---
+++ src/sim_tst/GISGenerateOperations.java
@@ -0,0 +1,85 @@
+package sim_tst;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+import p_graph_service.sim.PGraphDatabaseServiceSIM;
+
+import simulator.OperationFactory;
+import simulator.Simulator;
+import simulator.gis.OperationFactoryGIS;
+import simulator.gis.SimulatorGIS;
+
+public class GISGenerateOperations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Params: LogOutputPath DBDirectory AddRatio DelRatio LocalRatio
+		// GlobalRatio OpCount
+		// E.g. logs-output/log-gis-romania-LOCAL_10000.txt var/ 0 0 1 0 10000
+
+		if (args[0].equals("help")) {
+			System.out.println("Params - " + "LogOutputPath:Str "
+					+ "DBDirectory:Str " + "AddRatio:Double "
+					+ "DelRatio:Double " + "LocalRatio:Double"
+					+ "GlobalRatio:Double" + "OpCount:Int");
+		}
+
+		String logOutputPath = args[0];
+
+		String dbDir = args[1];
+
+		Double addRatio = Double.parseDouble(args[2]);
+
+		Double delRatio = Double.parseDouble(args[3]);
+
+		Double localRatio = Double.parseDouble(args[4]);
+
+		Double globalRatio = Double.parseDouble(args[5]);
+
+		Long opCount = Long.parseLong(args[6]);
+
+		// ****************
+
+		logOutputPath = "var/gis/logs-output/log-gis-romania-LOCAL_10000.txt";
+		dbDir = "var/gis/romania-BAL2-GID-NAME-COORDS-ALL_RELS/";
+		addRatio = 0d;
+		delRatio = 0d;
+		localRatio = 1d;
+		globalRatio = 0d;
+		opCount = 500l;
+
+		// ****************
+
+		long startTime = System.currentTimeMillis();
+
+		System.out.printf("Loading DB...");
+
+		GraphDatabaseService db = new PGraphDatabaseServiceSIM(dbDir, 0);
+
+		System.out.printf("%s", getTimeStr(System.currentTimeMillis()
+				- startTime));
+
+		OperationFactory operationFactory = new OperationFactoryGIS(db,
+				addRatio, delRatio, localRatio, globalRatio, opCount);
+
+		Simulator sim = new SimulatorGIS(db, logOutputPath, operationFactory);
+		sim.startSIM();
+
+		System.out.println("SLUT");
+
+		db.shutdown();
+
+	}
+
+	private static String getTimeStr(long msTotal) {
+		long ms = msTotal % 1000;
+		long s = (msTotal / 1000) % 60;
+		long m = (msTotal / 1000) / 60;
+
+		return String.format("%d(m):%d(s):%d(ms)%n", m, s, ms);
+	}
+
+}
---
+++ src/sim_tst/GISLoadOperations.java
@@ -0,0 +1,71 @@
+package sim_tst;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+import p_graph_service.sim.PGraphDatabaseServiceSIM;
+
+import simulator.OperationFactory;
+import simulator.Simulator;
+import simulator.gis.LogOperationFactoryGIS;
+import simulator.gis.SimulatorGIS;
+
+public class GISLoadOperations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Params: LogInputPath LogOutputPath DBDirectory
+		// E.g. logs-input/log-gis-romania-GLOBAL_500.txt
+		// logs-output/log-gis-romania-GLOBAL_500_Results.txt var/
+
+		if (args[0].equals("help")) {
+			System.out.println("Params - " + "LogInputPath:Str "
+					+ "LogOutputPath:Str " + "DBDirectory:Str ");
+		}
+
+		String logInputPath = args[0];
+
+		String logOutputPath = args[1];
+
+		String dbDir = args[2];
+
+		// ****************
+
+		logInputPath = "var/gis/logs-input/log-gis-romania-GLOBAL_500.txt";
+		logOutputPath = "var/gis/logs-output/log-gis-romania-GLOBAL_500_Results.txt";
+		dbDir = "var/gis/romania-BAL2-GID-NAME-COORDS-ALL_RELS";
+
+		// ****************
+
+		long startTime = System.currentTimeMillis();
+
+		System.out.printf("Loading DB...");
+
+		GraphDatabaseService db = new PGraphDatabaseServiceSIM(dbDir, 0);
+
+		System.out.printf("%s", getTimeStr(System.currentTimeMillis()
+				- startTime));
+
+		OperationFactory operationFactory = new LogOperationFactoryGIS(
+				logInputPath);
+
+		Simulator sim = new SimulatorGIS(db, logOutputPath, operationFactory);
+		sim.startSIM();
+
+		System.out.println("SLUT");
+
+		db.shutdown();
+
+	}
+
+	private static String getTimeStr(long msTotal) {
+		long ms = msTotal % 1000;
+		long s = (msTotal / 1000) % 60;
+		long m = (msTotal / 1000) / 60;
+
+		return String.format("%d(m):%d(s):%d(ms)%n", m, s, ms);
+	}
+
+}
